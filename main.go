ee9bc937fa4000abc379f427e39f981883fdebe3
NeoGitCrt1 <ysy20121223@live.com>
2021-06-03 17:49:59 +0900

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 	"sync"
 )
 
@@ -16,11 +17,13 @@ var reg *regexp.Regexp
 var ch chan string = make(chan string, 200)
 var wg *sync.WaitGroup = &sync.WaitGroup{}
 var pos int
+var extExcluded string
 func main() {
 	rootPath := flag.String("root", "C:/work/f", "search root")
 	outfile := flag.String("out", "C:/work/tmp/r.txt", "search result output file fullname")
 	regExp := flag.String("reg", `MailMapper\.getMailBody - ==> Parameters:\s(?P<name>.*)\(String\)`, "regex for search line")
 	flag.IntVar(&pos, "p", 1, "index of the capture group, -1 for whole line")
+	flag.StringVar(&extExcluded, "exclude", ".zip,.exe", "exclude file extension. eg. .zip,.exe")
 	flag.Parse()
 	reg = regexp.MustCompile(*regExp)
 	
@@ -71,6 +74,13 @@ func scan(fn string) {
 
 
 func iterate(path *string, searchMailId func(fn string), wg *sync.WaitGroup) {
+	var member void
+	set := make(map[string]void)
+	exclusive := strings.Split(extExcluded, ",")
+	for v := range exclusive {
+		set[exclusive[v]] = member
+	}
+
 	filepath.WalkDir(*path, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			log.Fatalf(err.Error())
@@ -80,9 +90,11 @@ func iterate(path *string, searchMailId func(fn string), wg *sync.WaitGroup) {
 			log.Printf("------------------------------------ %s", path)
 			return nil
 		}
-		if filepath.Ext(d.Name()) == ".zip" {
+		_, exists := set[filepath.Ext(d.Name())]
+		if exists {
 			return nil
 		}
+		
 		log.Printf(">> %s", d.Name())
 		wg.Add(1)
 		go searchMailId(path)
